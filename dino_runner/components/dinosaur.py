9b9e4abf2c0286adba1baeb310e375054bd51ea0
Kevin-Torrejon-30 <117110463+Mirko4Dead@users.noreply.github.com>
2022-10-31 21:49:55 -0400

    Clase 1: Tarea
    En este commit esta subido la tarea de hacer que el dinosaurio pueda agacharse

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,6 +1,6 @@
 import pygame
 from pygame.sprite import Sprite
-from dino_runner.utils.constants import JUMPING, RUNNING
+from dino_runner.utils.constants import DUCKING, JUMPING, RUNNING
 
 
 class Dinosaur(Sprite):
@@ -15,6 +15,7 @@ class Dinosaur(Sprite):
         self.step_index = 0
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
         self.jump_velocity = self.JUMP_VELOCITY
 
     def update(self, user_input):
@@ -22,13 +23,20 @@ class Dinosaur(Sprite):
             self.run()
         elif self.dino_jump:
             self.jump()
+        elif self.dino_duck:
+            self.duck()
 
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_duck = True
+            self.dino_run = False
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
+            self.dino_duck = False
+
         if self.step_index >= 10:
             self.step_index = 0
 
@@ -48,6 +56,11 @@ class Dinosaur(Sprite):
             self.dino_jump = False
             self.dino_rect.y = self.Y_POS
             self.jump_velocity = self.JUMP_VELOCITY
+
+    def duck(self):
+        self.image = DUCKING[0]if self.step_index < 5 else DUCKING[1]
+        self.dino_rect.y = self.Y_POS + 40
+        self.step_index += 1
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
